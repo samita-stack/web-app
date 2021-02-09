297d27cacb722e7ca149a673c62dd1756e405e88
samita-stack <78493668+samita-stack@users.noreply.github.com>
2021-02-09 17:24:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,11 @@ app.get("/expenses", requiresAuth(), async (req, res, next) => {
    });
    // 👆 end of changes 👆
    
+    res.render("expenses", {
+     user: req.oidc && req.oidc.user,
+     expenses: expenses.data,
+   });
+   
    });
  } catch (err) {
    next(err);
